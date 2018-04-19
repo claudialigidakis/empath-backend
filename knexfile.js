a9c5863f95ed081e0606a03e44bc6a18e0b2c0bd
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-04-18 18:35:22 -0700

    set up db and packages

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+const DB_NAME = 'empath'
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: `postgresql://localhost/${DB_NAME}_dev`,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  test: {
+    client: 'postgresql',
+    connection: `postgresql://localhost/${DB_NAME}_test`,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
+};
